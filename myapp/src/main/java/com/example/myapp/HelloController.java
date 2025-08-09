a4562b40576254e5b0405047a84e5106082553c7
javeriya01 <150579718+javeriya01@users.noreply.github.com>
2025-08-09 14:07:02 +0000

    first java app

---
+++ myapp/src/main/java/com/example/myapp/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.myapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.net.InetAddress;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        
+            return "Hello from myapp j ";
+        
+    }
+
+    
+}
